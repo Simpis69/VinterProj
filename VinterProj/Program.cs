664ba30dd99916aad8cd8823a47d1dbf59909c5d
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2023-12-01 16:16:33 +0100

    enemies

--- VinterProj/Program.cs
+++ VinterProj/Program.cs
@@ -3,25 +3,27 @@ using System.ComponentModel;
 using System.Data;
 using System.Numerics; //gör så att man kan använda vectorer
 
-Raylib.InitWindow(1000,750, "Vinter Projekt");
+Raylib.InitWindow(1000,750, "Vinter Projekt");  //betsämemr storleken av fönstret  
 Raylib.SetTargetFPS(60);
 
 
+Vector2 enemyPosition = Vector2.Zero;
+float enemyRadius = 20;
+Vector2 enemyVelocity = new Vector2(2, 2);
+Vector2[] linearPath = new Vector2[] { new Vector2(100, 120), new Vector2(100, 650)};
+int currentPathIndex = 0;
+
 
   
-Vector2 movement = new  Vector2(10, 10);
-Vector2 position = new Vector2(400, 300);
+Vector2 movement = new  Vector2(4, 4);  //bestämmer hastigheten som din karaktär rör sig i
+Vector2 position = new Vector2(400, 300); 
+Vector2 goalPos = new Vector2(0, 0);
 
-Rectangle characterRect = new Rectangle(10,10,32,32);
 
+Rectangle characterRect = new Rectangle(100,330,32,32); //bestämmer punkten som du spawnar på och storleken av karaktären
 List<Rectangle> walls = new();   
 
-
-Vector2 goalPos = new Vector2(0, 0);
 float goalSize = 25;
-
-
-
 string scene = "start";
 
 // int points = 0;
@@ -66,13 +68,29 @@ void DrawLevel(int[][] level)
             else if (level[row][column] == 2)
             {
                 Rectangle block = new Rectangle(column * 50, row * 50, 50, 50);
-                Raylib.DrawRectangleRec(block, Color.BROWN);
-                // CheckCollisions(player, block, "wall");
+                Raylib.DrawRectangleRec(block, Color.GRAY);
+                Raylib.CheckCollisionRecs(characterRect, block);
             }     
         }
     }
 }
 
+
+
+void UpdateLinearMovement(Vector2[] path, ref int currentIndex, ref Vector2 position, float velocity)
+{
+    if (currentIndex < path.Length - 1)
+    {
+        Vector2 target = path[currentIndex + 1];
+        Vector2 direction = Vector2.Normalize(target - position);
+        position += direction * velocity;
+
+        if (Vector2.Distance(position, target) < velocity)
+        {
+            currentIndex++;
+        }
+    }
+}
 
 while (!Raylib.WindowShouldClose())
 {
@@ -89,11 +107,8 @@ while (!Raylib.WindowShouldClose())
          }
     }
     // movement WASD
-    else if(scene == "start")
+    else if(scene == "game")
   {
-    movement = Vector2.Zero;}
-
-    
         if(Raylib.IsKeyDown(KeyboardKey.KEY_W))
        { characterRect.Y -= movement.Y;  
     }
@@ -111,6 +126,14 @@ while (!Raylib.WindowShouldClose())
     }
 
 
+   
+      if (scene == "game" && enemyPosition == Vector2.Zero)
+    {
+        enemyPosition = linearPath[0];
+    }
+
+
+  }
 
 // ------------------------------------------------------------------------------------------------------------------------------------  
 //                                                       RENDERING
@@ -132,10 +155,17 @@ DrawLevel(level1);
 
 
 
-// foreach (Rectangle wall in walls)
-// {
-//     Raylib.DrawRectangleRec(wall, Color.BLACK);
-// }
+
+
+if (scene == "game")
+    {
+        UpdateLinearMovement(linearPath, ref currentPathIndex, ref enemyPosition, enemyVelocity);
+
+        Raylib.DrawCircle((int)enemyPosition.X, (int)enemyPosition.Y, enemyRadius, Color.ORANGE);
+    }
+
+
+
 
 
 }
